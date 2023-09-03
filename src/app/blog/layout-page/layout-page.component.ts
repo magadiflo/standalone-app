365b377fb0ae187374fd53f4b0928fa7f0f6ca66
Martín <magadiflo@gmail.com>
2023-09-03 13:11:36 -0500

    Creando primeros componentes standalone

---
+++ src/app/blog/layout-page/layout-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from '../shared/header/header.component';
+
+@Component({
+  selector: 'blog-layout-page',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent],
+  templateUrl: './layout-page.component.html',
+  styleUrls: ['./layout-page.component.scss']
+})
+export class LayoutPageComponent {
+
+}
