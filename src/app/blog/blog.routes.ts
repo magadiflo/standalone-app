c6b7f5b5fe1705e4767e6b44c0597b80ea5e67e9
Martín <magadiflo@gmail.com>
2023-09-03 20:17:28 -0500

    Agregando rutas al header nav

--- src/app/blog/blog.routes.ts
+++ src/app/blog/blog.routes.ts
@@ -1,18 +1,24 @@
 import { Routes } from "@angular/router";
 
 import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
-import { HomePageComponent } from "./pages/home-page/home-page.component";
-import { DocsPageComponent } from "./pages/docs-page/docs-page.component";
-import { AboutPageComponent } from "./pages/about-page/about-page.component";
 
 export default [
   {
     path: '',
     component: LayoutPageComponent,
     children: [
-      { path: 'home', component: HomePageComponent, },
-      { path: 'docs', component: DocsPageComponent, },
-      { path: 'about', component: AboutPageComponent, },
+      {
+        path: 'home',
+        loadComponent: () => import('./pages/home-page/home-page.component').then(c => c.HomePageComponent),
+      },
+      {
+        path: 'docs',
+        loadComponent: () => import('./pages/docs-page/docs-page.component').then(c => c.DocsPageComponent),
+      },
+      {
+        path: 'about',
+        loadComponent: () => import('./pages/about-page/about-page.component').then(c => c.AboutPageComponent),
+      },
       { path: '**', redirectTo: 'home', },
     ]
   }
--- src/app/blog/shared/header/header.component.ts
+++ src/app/blog/shared/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'blog-shared-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
